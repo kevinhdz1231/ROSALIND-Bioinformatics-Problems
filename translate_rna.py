f062a27a2e79a759084f3658e1345a0d51fa4109
kevinhdz1231 <kevinhernandez1231@gmail.com>
2018-05-12 14:39:32 -0700

    Add files via upload

---
+++ translate_rna.py
@@ -0,0 +1,15 @@
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
+
+with open('rosalind_prot.txt', 'r') as mySeq:
+    rnaSeq = Seq((mySeq.read().replace('\n', "")), IUPAC.unambiguous_rna)
+    mySeq.close()
+
+proteinSeq = rnaSeq.translate()
+
+with open('translate_rna_solution.txt', 'w') as protFile:
+    stringSeq = str(proteinSeq)
+    stringSeq = stringSeq.replace("*", "") # For some reason Biopython adds a "*" to the end of a translated protein sequence. This line fixes that.
+    protFile.write(stringSeq)
+    protFile.close()
+
