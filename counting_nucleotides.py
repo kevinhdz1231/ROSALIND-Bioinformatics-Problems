04a8a462c3660f2216d9a8643c3ac27dfef5270b
kevinhdz1231 <kevinhernandez1231@gmail.com>
2018-05-12 00:16:41 -0700

    Add files via upload

---
+++ counting_nucleotides.py
@@ -0,0 +1,9 @@
+# This will count individual nucleotides
+with open('rosalind_dna.txt') as seqfile:
+    read_data = seqfile.read().rstrip()
+    seq_dict = {"A":0,"C":0,"G":0,"T":0}
+    for i in read_data:
+        seq_dict[i] += 1
+
+    for i in sorted(seq_dict.keys()):
+        print (i, seq_dict[i])
---
+++ dna_to_rna.py
@@ -0,0 +1,15 @@
+# change dna sequence to rna sequence
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
+
+with open('rosalind_rna.txt') as dna_seq:
+    seq_dna = Seq(dna_seq.read().rstrip(), IUPAC.unambiguous_dna)
+    dna_seq.close()
+
+with open('rosalind_rna_solution.txt', 'w') as rna_seq:
+    seq_rna = seq_dna.transcribe()
+    rna_seq.write(str(seq_rna))
+    rna_seq.close()
+
+
+
