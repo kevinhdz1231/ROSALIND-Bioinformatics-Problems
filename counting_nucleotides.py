f4d19bf2f750d94c16d902a658947288bbd768da
kevinhdz1231 <kevinhernandez1231@gmail.com>
2018-05-12 00:13:45 -0700

    Delete counting_nucleotides.py

--- counting_nucleotides.py
+++
@@ -1,9 +0,0 @@
-# This will count individual nucleotides
-with open('C:/Users/kevin/Desktop/Python_files/rosalind_dna.txt') as seqfile:
-    read_data = seqfile.read().rstrip()
-    seq_dict = {"A":0,"C":0,"G":0,"T":0}
-    for i in read_data:
-        seq_dict[i] += 1
-
-    for i in sorted(seq_dict.keys()):
-        print (i, seq_dict[i])
